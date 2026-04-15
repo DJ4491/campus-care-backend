bef0e35a76e34fee295b0dfaecaaf0fcb2a59de2
Dhruv Joshi <100696814+DJ4491@users.noreply.github.com>
2026-04-15 15:21:20 +0530

    refactor: update .gitignore to exclude service account key and enhance security

--- app/routes/posts_you_may_like.py
+++ app/routes/posts_you_may_like.py
@@ -1,20 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from firebase_admin import auth
+
 from app.crud.recommendations import get_posts_recommendations_for_user
-from fastapi import Depends
-from app.deps.auth import get_current_user
-from fastapi import HTTPException
 
 router = APIRouter()
+security = HTTPBearer()
+
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+
+    if not token:
+        raise HTTPException(status_code=401, detail="Missing token")
+
+    try:
+        decoded_token = auth.verify_id_token(token)
+        return decoded_token["uid"]
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=f"Invalid or expired token: {str(e)}")
 
 
 @router.get("/", summary="Mutual Friends")
-async def get_posts(user_id = Depends(get_current_user)):  # current_user = Depends(get_current_user)
+async def get_posts(user_id: str = Depends(get_current_user)):
     try:
-        # user_id = "IJLuAHXBs6gfbaleDHSvkQtn79I3"
-        return get_posts_recommendations_for_user(user_id)
+        posts = get_posts_recommendations_for_user(user_id)
+        return posts
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except KeyError:
         raise HTTPException(status_code=404, detail="User not found")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
