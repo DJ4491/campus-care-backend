c1ebe4e06a5692a6b832499ad78e982465280dd9
Dhruv Joshi <100696814+DJ4491@users.noreply.github.com>
2026-04-15 14:55:04 +0530

    refactor: uncomment and update recommendations and posts routes for improved functionality

--- app/crud/recommendations.py
+++ app/crud/recommendations.py
@@ -1,18 +1,15 @@
-# # Placeholder: we will call rec_engine here
-# from app.services.rec_engine import simple_mutual_rec, simple_page_rec
-# # from app.services.firebase_service import firestore
-# from app.services.firebase_service import db
+# Placeholder: we will call rec_engine here
+from app.services.rec_engine import simple_mutual_rec, simple_page_rec
+
+# from app.services.firebase_service import firestore
+from app.services.firebase_service import db
+from firebase_admin import firestore
 
 # def get_mutual_friend_recommendations_for_user(user_id):
 
 #     try:
 #         data = {}
-#         response = (
-#             service_supabase.table("profiles")
-#             .select("id, created_at, username")
-#             .order("created_at", desc=True)
-#             .execute()
-#         )
+#         response = db.collection("posts").order_by('created_at',direction = firestore.Query.DESCENDING)
 
 #         friendship_response = (
 #             service_supabase.table("friendships")
@@ -36,52 +33,75 @@
 #         return f"There was a problem: {e}"
 
 
-# def get_posts_recommendations_for_user(user_id):
+def get_posts_recommendations_for_user(user_id):
 
-#     try:
+    try:
 
-#         data = {}
-#         response = (
-#             service_supabase.table("profiles")
-#             .select("id,created_at,username")
-#             .order("created_at", desc=True)
-#             .execute()
-#         )
+        data = {}
+        response = (
+            db.collection("profiles")
+            .select(["id", "createdAt", "name"])
+            .order_by("createdAt", direction=firestore.Query.DESCENDING)
+        )
+        
+        profile_docs = response.stream()
+        
+        likes_response = db.collection("likes").select(["postId", "userId"])
+         
+        likes_docs = likes_response.stream()    
 
-#         likes_response = (
-#             service_supabase.table("likes").select("user_id,post_id").execute()
-#         )
+        posts_response = (
+            db.collection("posts")
+            .select(["created_at", "title","author.id"])
+            .order_by("created_at", direction=firestore.Query.DESCENDING)
+        )
 
-#         posts_response = (
-#             service_supabase.table("posts").select("id,author_id").execute()
-#         )
+        docs = posts_response.stream()
 
-#         post_author_map = {}
+        posts_data = []
+        for doc in docs:
+            post_data = doc.to_dict()
+            post_data["id"] = doc.id
+            posts_data.append(post_data)
+        
+        users = []
+        for doc in profile_docs:
+            user_data = doc.to_dict()
+            user_data["id"] = doc.id
+            users.append(user_data)
+        
+        likes = []
+        for doc in likes_docs:
+            like_data = doc.to_dict()
+            likes.append(like_data)
+        
+        
+        post_author_map = {}
 
-#         for post in posts_response.data:
-#             post_id = post["id"]
-#             author_id = post["author_id"]
-#             post_author_map[post_id] = author_id
+        for post in posts_data:
+            post_id = post["id"]
+            author_id = post.get('author',{}).get('id')
+            post_author_map[post_id] = author_id
 
-#         created_by_user = set()
+        created_by_user = set()
 
-#         for p_id, auth_id in post_author_map.items():
-#             if auth_id == user_id:
-#                 created_by_user.add(p_id)
+        for p_id, auth_id in post_author_map.items():
+            if auth_id == user_id:
+                created_by_user.add(p_id)
 
-#         data["created_by_user"] = created_by_user
+        data["created_by_user"] = created_by_user
 
-#         likes_data = likes_response.data or []
-#         data["users"] = response.data or []
+        likes_data = likes or []
+        data["users"] = users or []
 
-#         for user in data["users"]:
-#             if "liked_posts" not in user:
-#                 user["liked_posts"] = []
+        for user in data["users"]:
+            if "liked_posts" not in user:
+                user["liked_posts"] = []
 
-#             for post in likes_data:
-#                 if user["id"] == post["user_id"]:
-#                     user["liked_posts"].append(post["post_id"])
+            for post in likes_data:
+                if user["id"] == post["userId"]:
+                    user["liked_posts"].append(post["postId"])
 
-#         return simple_page_rec(user_id, data)
-#     except Exception as e:
-#         return f"There was a problem: {e}"
+        return simple_page_rec(user_id, data)
+    except Exception as e:
+        return f"There was a problem: {e}"
